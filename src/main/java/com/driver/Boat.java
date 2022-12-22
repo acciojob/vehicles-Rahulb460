a7c40b1651644eaaf72cbc21bfd554e2a99d4aa0
Rahul <rahulbhatt460@gmail.com>
2022-12-22 12:40:25 +0530

    Code added

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,14 @@ public class Boat implements WaterVehicle{
     String name;
     int capacity;
 
+    public Boat() {
+
+    }
+   public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
